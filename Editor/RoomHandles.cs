112cd1b41409fa036b69b3e88e2c607fc29784e7
kichriwa <kira.wanjek@gmx.de>
2020-07-18 00:43:06 +0200

    Feat: Filter selection while tool is active

--- Editor/RoomHandles.cs
+++ Editor/RoomHandles.cs
@@ -1,8 +1,13 @@
-﻿using UnityEditor;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEditor;
 using UnityEngine;
 using UnityLevelEditor.Model;
 using UnityEditor.EditorTools;
 using UnityEngine.Serialization;
+using UnityEngine.UI;
+using Object = UnityEngine.Object;
 
 namespace UnityLevelEditor.RoomExtension
 {
@@ -30,12 +35,12 @@ namespace UnityLevelEditor.RoomExtension
         {
             EditorTools.activeToolChanged += ChangeIcon;
             ChangeIcon();
-            //Debug.Log("Enable Tool");
+            Debug.Log("Enable Tool");
         }
 
         private void OnDisable()
         {
-            //Debug.Log("Disable");
+            Debug.Log("Disable");
             EditorTools.activeToolChanged -= ChangeIcon;
         }
 
@@ -55,9 +60,7 @@ namespace UnityLevelEditor.RoomExtension
         #endregion
 
         #region Handles
-
-        private RoomElement roomElement;
-
+        
         private float capOffset = 3f;
 
         private float capSize = 2f;
@@ -69,13 +72,13 @@ namespace UnityLevelEditor.RoomExtension
                 return false;
             }
 
-            roomElement = t.GetComponent<RoomElement>();
+            var roomElement = t.GetComponent<RoomElement>();
             return (roomElement != null && roomElement.Type == RoomElementTyp.Wall);
         }
 
-        private Vector3 Snapping(Vector3 pos)
+        private Vector3 Snapping(ElementSpawner floorSpawner, Vector3 pos)
         {
-            var snapValue = roomElement.ExtendableRoom.FloorSpawner.Bounds.size.x;
+            var snapValue = floorSpawner.Bounds.size.x;
             
             Debug.Log(snapValue);
             pos.x = Mathf.Round(pos.x / snapValue) * snapValue;
@@ -85,7 +88,7 @@ namespace UnityLevelEditor.RoomExtension
 
         public override void OnToolGUI(EditorWindow window)
         {
-            if (!roomElement || roomElement.Type != RoomElementTyp.Wall)
+            if (!TryGetSelectedWallsAndUpdateSelection(out var selectedWalls))
             {
                 return;
             }
@@ -93,8 +96,9 @@ namespace UnityLevelEditor.RoomExtension
             EditorGUI.BeginChangeCheck();
 
             Vector3 position = Tools.handlePosition;
+            var representativeWall = selectedWalls[0];
 
-            switch (roomElement.SpawnOrientation)
+            switch (representativeWall.SpawnOrientation)
             {
                 case SpawnOrientation.Back:
                     position = DrawHandle(position, Vector3.back, Color.blue);
@@ -112,12 +116,18 @@ namespace UnityLevelEditor.RoomExtension
 
             if (EditorGUI.EndChangeCheck())
             {
-                Vector3 delta = Snapping(position - Tools.handlePosition);
+               ChangePosition( Snapping(representativeWall.ExtendableRoom.FloorSpawner, position - Tools.handlePosition));
+            }
+        }
 
-                Undo.RecordObjects(Selection.transforms, "Room Extension");
 
-                foreach (var transform in Selection.transforms)
-                    transform.position += delta;
+        private void ChangePosition(Vector3 delta)
+        {
+            Undo.RecordObjects(Selection.transforms, "Room Extension");
+
+            foreach (var transform in Selection.transforms)
+            {
+                transform.position += delta;
             }
         }
 
@@ -129,6 +139,73 @@ namespace UnityLevelEditor.RoomExtension
             }
         }
 
+        #endregion
+        
+        
+        #region SelectionHandling
+
+        private bool TryGetSelectedWallsAndUpdateSelection(out List<RoomElement> selectedWalls)
+        {
+            selectedWalls = null;
+            
+            if (!TryGetSelectedGameObject(out var selectedGameObject))
+            {
+                return false;
+            }
+            
+            var selectedRoomElement = selectedGameObject.GetComponent<RoomElement>();
+
+            if (selectedRoomElement == null)
+            {
+                RemoveRoomElementsFromSelection();
+                return false;
+            }
+
+            var roomElementsOfActiveType = FilterSelectionForRoomElementsOfGivenTypeAndOrientation(selectedRoomElement.Type, selectedRoomElement.SpawnOrientation);
+            
+            Selection.objects = roomElementsOfActiveType.Select(roomElement => roomElement.gameObject).ToArray<Object>();
+
+            if (selectedRoomElement.Type != RoomElementTyp.Wall)
+            {
+                return false;
+            }
+
+            selectedWalls = roomElementsOfActiveType;
+            return true;
+        }
+
+        private bool TryGetSelectedGameObject(out GameObject selectedGameObject)
+        {
+            selectedGameObject = null;
+            
+            if (Selection.objects.Length <= 0)
+            {
+                return false;
+            }
+            
+            var mostRecentSelected = Selection.objects[Selection.objects.Length - 1];
+
+            if (mostRecentSelected is GameObject gameObject)
+            {
+                selectedGameObject = gameObject;
+                return true;
+            }
+            
+            return false;
+        }
+
+        private void RemoveRoomElementsFromSelection()
+        {
+            Selection.objects = Selection.gameObjects.Where(go => go.GetComponent<RoomElement>() == null).ToArray<Object>();
+        }
+
+        private List<RoomElement> FilterSelectionForRoomElementsOfGivenTypeAndOrientation(RoomElementTyp type, SpawnOrientation orientation)
+        {
+            return Selection.transforms.Select(t => t.GetComponent<RoomElement>())
+                .Where(r => r != null && r.Type == type && r.SpawnOrientation == orientation).ToList();
+        }
+        
+        
         #endregion
     }
 }
