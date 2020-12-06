26cffb211f1d417311917024c5f689653230d0d3
Kira Wanjek <kira.wanjek@gmx.de>
2020-12-06 18:08:23 +0100

    fix: Direction isn't contained in a namespace

--- Runtime/Direction.cs
+++ Runtime/Direction.cs
@@ -1,79 +1,84 @@
 ﻿using System;
 using UnityLevelEditor.Model;
 
-public enum Direction
+namespace UnityLevelEditor
 {
-    Front,
-    Right,
-    Back,
-    Left
-}
-
-public static class DirectionExtensions
-{
-    private static readonly int DirectionEnumLength = Enum.GetValues(typeof(Direction)).Length;
-
-    public static SpawnOrientation ToSpawnOrientation(this Direction direction)
+    public enum Direction
     {
-        switch (direction)
-        {
-            case Direction.Front:
-                return SpawnOrientation.Front;
-            case Direction.Right:
-                return SpawnOrientation.Right;
-            case Direction.Back:
-                return SpawnOrientation.Back;
-            case Direction.Left:
-                return SpawnOrientation.Left;
-            default:
-                throw new ArgumentOutOfRangeException(nameof(direction), direction, $"Converting '{direction}' to SpawnOrientation is not supported.");
-        }
+        Front,
+        Right,
+        Back,
+        Left
     }
 
-    public static Direction Shift(this Direction direction, int shiftBy)
+    public static class DirectionExtensions
     {
-        var index = (int) direction;
+        private static readonly int DirectionEnumLength = Enum.GetValues(typeof(Direction)).Length;
 
-        index += shiftBy;
-        index %= DirectionEnumLength;
-
-        if (index < 0)
+        public static SpawnOrientation ToSpawnOrientation(this Direction direction)
         {
-            index += DirectionEnumLength;
+            switch (direction)
+            {
+                case Direction.Front:
+                    return SpawnOrientation.Front;
+                case Direction.Right:
+                    return SpawnOrientation.Right;
+                case Direction.Back:
+                    return SpawnOrientation.Back;
+                case Direction.Left:
+                    return SpawnOrientation.Left;
+                default:
+                    throw new ArgumentOutOfRangeException(nameof(direction), direction,
+                        $"Converting '{direction}' to SpawnOrientation is not supported.");
+            }
         }
 
-        return (Direction) index;
-    }
-
-    public static Direction Shift(this Direction direction, bool clockwise)
-    {
-        return clockwise ? direction.Shift(1) : direction.Shift(-1);
-    }
-
-    public static bool IsSideways(this Direction direction)
-    {
-        return direction == Direction.Left || direction == Direction.Right;
-    }
-
-    public static bool TowardsNegative(this Direction direction)
-    {
-        return direction == Direction.Left || direction == Direction.Back;
-    }
-
-    public static Direction Opposite(this Direction direction)
-    {
-        switch (direction)
+        public static Direction Shift(this Direction direction, int shiftBy)
         {
-            case Direction.Back:
-                return Direction.Front;
-            case Direction.Front:
-                return Direction.Back;
-            case Direction.Left:
-                return Direction.Right;
-            case Direction.Right:
-                return Direction.Left;
-            default:
-                throw new ArgumentOutOfRangeException(nameof(direction), direction, $"Getting opposite for Direction '{direction}' is not supported.");
+            var index = (int) direction;
+
+            index += shiftBy;
+            index %= DirectionEnumLength;
+
+            if (index < 0)
+            {
+                index += DirectionEnumLength;
+            }
+
+            return (Direction) index;
+        }
+
+        public static Direction Shift(this Direction direction, bool clockwise)
+        {
+            return clockwise ? direction.Shift(1) : direction.Shift(-1);
+        }
+
+        public static bool IsSideways(this Direction direction)
+        {
+            return direction == Direction.Left || direction == Direction.Right;
+        }
+
+        public static bool TowardsNegative(this Direction direction)
+        {
+            return direction == Direction.Left || direction == Direction.Back;
+        }
+
+        public static Direction Opposite(this Direction direction)
+        {
+            switch (direction)
+            {
+                case Direction.Back:
+                    return Direction.Front;
+                case Direction.Front:
+                    return Direction.Back;
+                case Direction.Left:
+                    return Direction.Right;
+                case Direction.Right:
+                    return Direction.Left;
+                default:
+                    throw new ArgumentOutOfRangeException(nameof(direction), direction,
+                        $"Getting opposite for Direction '{direction}' is not supported.");
+            }
         }
     }
 }
